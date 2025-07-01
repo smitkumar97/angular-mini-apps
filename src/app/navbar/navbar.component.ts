0bd5160b18b1f340e6b5bf941393abb586a481bc
Smit Patel <smitkumar97@gmail.com>
2025-07-02 00:10:13 +0530

    removed unneccessary routes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { MenuItem } from 'primeng/api';
-import { AvatarModule } from 'primeng/avatar';
-import { BadgeModule } from 'primeng/badge';
-import { InputTextModule } from 'primeng/inputtext';
-import { Menubar } from 'primeng/menubar';
-import { StyleClassModule } from 'primeng/styleclass';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
+import { MenuItem } from "primeng/api";
+import { AvatarModule } from "primeng/avatar";
+import { BadgeModule } from "primeng/badge";
+import { InputTextModule } from "primeng/inputtext";
+import { Menubar } from "primeng/menubar";
+import { StyleClassModule } from "primeng/styleclass";
 
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   imports: [
     Menubar,
     AvatarModule,
@@ -19,8 +19,8 @@ import { StyleClassModule } from 'primeng/styleclass';
     BadgeModule,
     StyleClassModule,
   ],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.scss",
 })
 export class NavbarComponent {
   items: MenuItem[] | undefined;
@@ -28,27 +28,23 @@ export class NavbarComponent {
   ngOnInit() {
     this.items = [
       {
-        label: 'Home',
-        icon: 'pi pi-home',
-        route: '/home',
+        label: "Home",
+        icon: "pi pi-home",
+        route: "/home",
       },
+      // {
+      //   label: 'Contact',
+      //   icon: 'pi pi-phone',
+      //   route: '/contact',
+      // },
       {
-        label: 'Contact',
-        icon: 'pi pi-phone',
-        route: '/contact',
-      },
-      {
-        label: 'Projects',
-        icon: 'pi pi-home',
-        badge: '3',
+        label: "Projects",
+        icon: "pi pi-home",
+        badge: "3",
         items: [
           {
-            label: 'Todo App',
-            url: '/todo',
-          },
-          {
-            label: 'Vite.js',
-            url: '/reset',
+            label: "Todo App",
+            url: "/todo",
           },
         ],
       },
