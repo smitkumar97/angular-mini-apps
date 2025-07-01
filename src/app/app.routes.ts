a5d5aefca4d08d4dac7022a7887f20c607c503a3
Smit Patel <smitkumar97@gmail.com>
2025-07-02 00:08:04 +0530

    removed register and reset components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,58 +1,47 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { FormDemoComponent } from './form-demo/form-demo.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { TestComponent } from './test/test.component';
-import { TodoComponent } from './todo/todo.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { FormDemoComponent } from "./form-demo/form-demo.component";
+import { CalculatorComponent } from "./calculator/calculator.component";
+import { TestComponent } from "./test/test.component";
+import { TodoComponent } from "./todo/todo.component";
 
 export const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomeComponent, // Empty path
-    title: 'Home Page',
+    title: "Home Page",
   },
   {
-    path: 'home',
+    path: "home",
     component: HomeComponent, // Empty path
-    title: 'Home Page',
+    title: "Home Page",
   },
   {
-    path: 'todo',
+    path: "todo",
     component: TodoComponent, // Static, most specific
-    title: 'Todo App',
+    title: "Todo App",
   },
   {
-    path: 'register',
-    component: RegisterComponent, // Static, most specific
-    title: 'Registration Page',
-  },
-  {
-    path: 'reset',
-    component: ResetPasswordComponent, // Static, most specific
-  },
-  {
-    path: 'calculator',
+    path: "calculator",
     component: CalculatorComponent, // Static, most specific
-    title: 'Calculator App',
+    title: "Calculator App",
   },
   {
-    path: 'test',
+    path: "test",
     component: TestComponent, // Static, most specific
-    title: 'Test App',
+    title: "Test App",
   },
   {
-    path: 'users/:id',
+    path: "users/:id",
     component: FormDemoComponent, // Dynamic
   },
   {
-    path: 'users',
+    path: "users",
     component: FormDemoComponent, // Static, less specific
   },
   {
-    path: '**',
+    path: "**",
     component: NotFoundComponent, //Wildcard - always last
   },
 ];
