1b705a16b46d1f3315c0902f46c6dbc236c0e7bd
Smit Patel <smitkumar97@gmail.com>
2025-07-03 14:04:11 +0530

    login component css fixes

---
+++ src/app/models/todo.model.ts
@@ -0,0 +1,8 @@
+interface TodoItem {
+  id: number;
+  name: string;
+  isCompleted: boolean;
+  checked: boolean;
+}
+
+export default TodoItem;
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,36 +1,30 @@
-import { Component } from '@angular/core';
-import { TableModule } from 'primeng/table';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
-
-interface TodoItem {
-  id: number;
-  name: string;
-  isCompleted: boolean;
-  checked: boolean;
-}
+import { Component } from "@angular/core";
+import { TableModule } from "primeng/table";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { ButtonModule } from "primeng/button";
+import { ToastModule } from "primeng/toast";
+import { MessageService } from "primeng/api";
+import TodoItem from "../models/todo.model";
 
 @Component({
-  selector: 'app-todo',
+  selector: "app-todo",
   imports: [TableModule, ButtonModule, ToastModule, FormsModule, CommonModule],
   providers: [MessageService],
-  templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss',
+  templateUrl: "./todo.component.html",
+  styleUrl: "./todo.component.scss",
 })
 export class TodoComponent {
   todoList: TodoItem[] = [
     {
       id: 1,
-      name: 'Design a Todo App',
+      name: "Design a Todo App",
       isCompleted: false,
       checked: false,
     },
     {
       id: 2,
-      name: 'Build a Weather App',
+      name: "Build a Weather App",
       isCompleted: false,
       checked: false,
     },
@@ -44,9 +38,9 @@ export class TodoComponent {
   addTodoItem() {
     if (!this.todoItemName) {
       this.show({
-        severity: 'warn',
-        summary: 'Alert!',
-        detail: 'Please enter a the todo name',
+        severity: "warn",
+        summary: "Alert!",
+        detail: "Please enter a the todo name",
         life: 3000,
       });
       return;
@@ -60,11 +54,11 @@ export class TodoComponent {
     };
 
     this.todoList.push(todoItm);
-    this.todoItemName = '';
+    this.todoItemName = "";
     this.show({
-      severity: 'success',
-      summary: 'Success',
-      detail: 'Todo added successfully.',
+      severity: "success",
+      summary: "Success",
+      detail: "Todo added successfully.",
       life: 3000,
     });
   }
@@ -77,16 +71,16 @@ export class TodoComponent {
     if (this.selectedTodo.length === this.todoList.length) {
       this.todoList.length = 0;
       this.show({
-        severity: 'success',
-        summary: 'Success',
-        detail: 'Todos deleted successfully.',
+        severity: "success",
+        summary: "Success",
+        detail: "Todos deleted successfully.",
         life: 3000,
       });
     } else {
       this.show({
-        severity: 'warn',
-        summary: 'Alert!',
-        detail: 'Please select all todos.',
+        severity: "warn",
+        summary: "Alert!",
+        detail: "Please select all todos.",
         life: 3000,
       });
     }
