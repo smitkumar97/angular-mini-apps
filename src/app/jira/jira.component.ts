4e76792640a3c70e2a4f2ce55950199a79452dd2
Smit Patel <smitkumar97@gmail.com>
2025-07-22 16:31:25 +0530

    JIRA dashboard using angular DragDropModule has been implemented

--- src/app/jira/jira.component.ts
+++ src/app/jira/jira.component.ts
@@ -1,8 +1,6 @@
 import { Component } from "@angular/core";
-import { UserService } from "../services/user.service";
 import { CardModule } from "primeng/card";
 import { ButtonModule } from "primeng/button";
-import { InnerCardComponent } from "../inner-card/inner-card.component";
 import { CommonModule } from "@angular/common";
 import {
   CdkDragDrop,
@@ -11,62 +9,62 @@ import {
   transferArrayItem,
 } from "@angular/cdk/drag-drop";
 
-interface todo {
-  id: number;
-  todo: string;
-  completed: boolean;
-  userId: number;
+interface Ticket {
+  id: string;
+  title: string;
+  description: string;
+  status: "todo" | "in-progress" | "done";
+  type?: string;
+  points?: number;
 }
 
 @Component({
   selector: "app-jira",
-  imports: [
-    ButtonModule,
-    CardModule,
-    InnerCardComponent,
-    CommonModule,
-    DragDropModule,
-  ],
+  imports: [ButtonModule, CardModule, CommonModule, DragDropModule],
   templateUrl: "./jira.component.html",
   styleUrl: "./jira.component.scss",
 })
 export class JiraComponent {
-  todoList$: todo[] = [];
-  inprogress$: todo[] = [];
+  todo: Ticket[] = [
+    {
+      id: "PROJ-123",
+      title: "Implement user authentication",
+      description: "Create login and registration pages",
+      type: "Story",
+      status: "todo",
+      points: 5,
+    },
+    {
+      id: "PROJ-124",
+      title: "Implement user profile",
+      description: "Create user profile pages",
+      type: "Story",
+      status: "in-progress",
+      points: 5,
+    },
+    {
+      id: "PROJ-123",
+      title: "Implement table",
+      description: "Create table",
+      type: "Story",
+      status: "todo",
+      points: 5,
+    },
+    {
+      id: "PROJ-155",
+      title: "Implement dashboard",
+      description: "Create charts",
+      type: "Task",
+      status: "in-progress",
+      points: 5,
+    },
+  ];
 
-  constructor(private myService: UserService) {}
+  inProgress: Ticket[] = [];
+  inQA: Ticket[] = [];
+  done: Ticket[] = [];
 
-  ngOnInit(): void {
-    this.myService
-      .getTaskDetail()
-      .pipe()
-      .subscribe({
-        next: (response) => {
-          if (response.todos.length > 0) {
-            this.todoList$ = response.todos;
-          }
-        },
-        error: (error) => {
-          console.error(error);
-        },
-      });
-  }
-
-  dragoverHandler(ev: any) {
-    ev.preventDefault();
-  }
-
-  dropHandler(ev: any) {
-    ev.preventDefault();
-    const data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
-
-  dragstartHandler(ev: any) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-
-  drop(event: any) {
+  drop(event: CdkDragDrop<Ticket[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
